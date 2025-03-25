2c31823cf32c06b51bfe3caa335653ad1b2fd7d0
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-24 23:04:05 -0400

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from . import db  # Import db from __init__.py
 from flask_login import UserMixin
 import json
 from datetime import datetime
-
-db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
