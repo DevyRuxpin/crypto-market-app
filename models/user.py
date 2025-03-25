b8bd103d9a30b8bfeaa7eebe35928db9d302b9cf
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-24 21:01:23 -0400

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,8 @@
-from flask_login import UserMixin
 import uuid
+from flask_login import UserMixin
 
 class User(UserMixin):
-    # In-memory user storage for this demo
-    # In a real app, you would use a database
+    # In-memory storage for users (in a real app, this would be a database)
     users = {}
     
     def __init__(self, name, email, password_hash):
@@ -11,14 +10,25 @@ class User(UserMixin):
         self.name = name
         self.email = email
         self.password_hash = password_hash
+        self.settings = {
+            'theme': 'light',
+            'currency': 'USD',
+            'notification_enabled': True,
+            'default_crypto_list': ['BTC', 'ETH', 'BNB', 'XRP', 'ADA', 'SOL', 'DOT', 'DOGE']
+        }
         
-        # Save user to in-memory storage
+        # Save to in-memory storage
         User.users[self.id] = self
     
-    @staticmethod
-    def find_by_email(email):
-        """Find a user by email address"""
-        for user in User.users.values():
+    @classmethod
+    def find_by_email(cls, email):
+        for user in cls.users.values():
             if user.email == email:
                 return user
         return None
+    
+    def update_settings(self, new_settings):
+        for key, value in new_settings.items():
+            if key in self.settings:
+                self.settings[key] = value
+        return self.settings
