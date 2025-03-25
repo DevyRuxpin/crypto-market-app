164aae1e6417c8200566c54167163f825e765577
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-24 23:03:27 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,3 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Import models so they are registered with SQLAlchemy
-from .user import User
-from .portfolio import Portfolio, PortfolioItem
-from .alert import Alert
-from .watchlist import Watchlist, WatchlistSymbol
-
