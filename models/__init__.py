b5a624d30bc872ab98b77891b42ff888edc4c4b7
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-24 22:55:44 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -8,10 +8,3 @@ from .portfolio import Portfolio, PortfolioItem
 from .alert import Alert
 from .watchlist import Watchlist, WatchlistSymbol
 
-# Update model imports to use the shared db instance
-User.db = db
-Portfolio.db = db
-PortfolioItem.db = db
-Alert.db = db
-Watchlist.db = db
-WatchlistSymbol.db = db
