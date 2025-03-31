bd9356cb8493cd6f9ce20dd604230d0af8246fb0
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-31 15:47:54 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,6 @@
-# models/__init__.py
 from .user import User
 from .portfolio import Portfolio, PortfolioItem
 from .watchlist import Watchlist, WatchlistSymbol
 from .alert import Alert
 
-
-from database import db
-
-__all__ = [
-    'User',
-    'Portfolio',
-    'PortfolioItem',
-    'Watchlist',
-    'WatchlistSymbol',
-    'Alert'
-]
+from ..database import db
