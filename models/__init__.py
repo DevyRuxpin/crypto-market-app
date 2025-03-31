714de956ae4c434178819cf75dd6f8527817c0cf
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-31 15:40:52 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 # models/__init__.py
 from .user import User
+from .portfolio import Portfolio, PortfolioItem
 from .watchlist import Watchlist, WatchlistSymbol
 from .alert import Alert
+
 
 from database import db
 
