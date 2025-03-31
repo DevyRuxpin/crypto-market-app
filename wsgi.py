0ccc20ab17f9178ed70690f92dcf6bc3b3e7b8b2
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-31 15:48:57 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,12 @@
-# wsgi.py
 import os
-from app import create_app
+from app import create_app, db
 
 # Get environment from environment variable or default to production
 env = os.environ.get('FLASK_ENV', 'production')
 app = create_app(env)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
