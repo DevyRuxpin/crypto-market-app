a5a29ca21006f25d7d9a20b51ce7db4646cba8c3
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-04-01 15:44:33 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,5 @@ import os
 from app import app  # Use the app instance directly
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    
