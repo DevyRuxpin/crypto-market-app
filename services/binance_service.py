934b5cf4b607207082ceb1a564422c1496cf5c86
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-04-01 00:47:31 -0400

    Update binance_service.py

--- services/binance_service.py
+++ services/binance_service.py
@@ -4,8 +4,7 @@ from flask import current_app
 class BinanceService:
     def __init__(self):
         self.base_url = 'https://data-api.binance.vision/api/v3'
-        self.api_key = current_app.config['BINANCE_API_KEY']
-        self.secret_key = current_app.config['BINANCE_SECRET_KEY']
+        
 
     def get_ticker_data(self, symbol):
         url = f'{self.base_url}/ticker/24hr'
